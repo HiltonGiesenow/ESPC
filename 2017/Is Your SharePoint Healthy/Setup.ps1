08e6422db7413678efbdcd7cd5e093dc0bfd6c81
Hilton Giesenow <hilton@giesenow.com>
2017-11-05 14:05:07 +0200

    updating slides, adding setup script to prep screen for session

---
+++ 2017/Is Your SharePoint Healthy/Setup.ps1
@@ -0,0 +1,12 @@
+Start-Process "https://technet.microsoft.com/en-us/library/gg277248%28v=office.14%29.aspx"
+Start-Process "https://technet.microsoft.com/en-us/library/jj219701.aspx"
+Start-Process "https://collab365.community/forum/topics/what-is-on-your-sharepoint-admin-checklist/"
+Start-Process "https://gallery.technet.microsoft.com/office/Sharepoint-Checklist-Daily-56293835"
+Start-Process "https://technet.microsoft.com/en-us/library/ee748636%28v=office.16%29.aspx"
+
+Start-Process "C:\Download\oit2013-whitepaper-sharepoint-operations-checklist.docx"
+Start-Process "C:\Download\Checklist.xlsx"
+
+Start-Process "https://technet.microsoft.com/en-us/library/ff686816(v=office.16).aspx"
+
+Start-Process "http://powershellgallery.com/packages/PoShMon"
